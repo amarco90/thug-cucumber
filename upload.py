0e7528d7647564a2e459ef3a1f0e6550983854c3
Jan Wilken Dörrie <dojan@ethz.ch>
2016-09-17 12:20:30 +0200

    Fix Server

--- upload.py
+++ upload.py
@@ -34,29 +34,10 @@ def allowed_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
+        img = request.form['img']
+        return "Received Image of Size %d" % len(img)
+
+    return app.send_static_file('index.html')
 
 
 @app.route('/base64/<filename>')
